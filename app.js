24f5fdee5dee8f4d719df1477d809084eb1ffa31
João Oliveira <jvcbatist4@outlook.com>
2022-03-22 20:38:24 -0300

    Testing methods get/post/put/delete with Express

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 const express = require('express')
+const checklistRouter = require('./src/routes/checklist')
+
+const app = express()
+app.use(express.json())
+
+app.use('/checklists', checklistRouter)
+
+app.listen(3000, () => {
+  console.log('Server is ACTIVE!')
+})
+
